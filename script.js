05309150048bdb08aef8045a203f042339993764
TheGeniusOfTheGame <maxsafin14@gmail.com>
2023-04-02 16:25:18 +0300

    version 2.0

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const header = document.getElementById('header')
 const backToTop = document.getElementById('back-to-top')
+const menuButton = document.querySelector('.open-menu')
+const headerNav = document.querySelector('.header-nav')
+let opened
 let oldScrollTop = 0
 
+menuButton.addEventListener("click", () => {
+    if (headerNav.classList.contains('opened')) {
+        headerNav.classList.remove('opened')
+        menuButton.classList.remove('changeIcon')
+        opened = false
+    }
+
+    else {
+        headerNav.classList.add('opened')
+        menuButton.classList.add('changeIcon')
+        backToTop.classList.remove("backToTopAnim")
+        opened = true
+    }
+
+})
+
+headerNav.addEventListener("click", () => {
+    if (opened) {
+        headerNav.classList.remove('opened')
+        menuButton.classList.remove('changeIcon')
+        opened = false
+    }
+
+    else {
+        headerNav.classList.add('opened')
+        menuButton.classList.add('changeIcon')
+        backToTop.classList.remove("backToTopAnim")
+        opened = true
+    }
+})
 
 document.addEventListener("scroll", () => {
     let scrollTop = window.scrollY
-    if (scrollTop > oldScrollTop) {
+    if (scrollTop > oldScrollTop && !opened) {
         header.classList.add("hide-header")
     }
     else  {
@@ -14,7 +47,7 @@ document.addEventListener("scroll", () => {
 
     oldScrollTop = scrollTop
 
-    if (scrollTop > 500) {
+    if (scrollTop > 500 && !opened) {
         backToTop.classList.add("backToTopAnim")
     }
 
